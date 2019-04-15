7505a5dfda73e75261603829f05d6b06378419a8
Deggsie73 <mattp.cartwright@gmail.com>
2019-04-15 20:17:45 +0100

    End of Section13

--- DatingApp.API/DTOs/UserForUpdateDto.cs
+++ DatingApp.API/DTOs/UserForUpdateDto.cs
@@ -3,6 +3,7 @@ namespace DatingApp.API.DTOs
     public class UserForUpdateDto
     {
         public string Introduction { get; set; }
+        public string KnownAs { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string City { get; set; }
